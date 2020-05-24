f830f0cba481d4b25bbd7a1575b57e8ab742ba42
kzy611 <mnkckzy@gmail.com>
2020-05-24 18:11:03 +0900

    Edit aggretage function.

--- expenses/tests.py
+++ expenses/tests.py
@@ -9,24 +9,25 @@ from django.test import TestCase
 def sorted_unique_list(lst):
     return sorted(list(set(lst)))
 
-# レコード集計用の辞書作成関数
-def create_dict(months, key, contents):
-    content_dict_list = [{key: content, 'amount': 0} for content in contents]
-    ret_dict = [{'month': month, 'content': content_dict_list} for month in months]
-    return ret_dict
-
 # レコード集計用、フィルタ&合計関数
-def filter_sum(dict, amounts, key, key_pk):
-    amount_filtered_month = filter(lambda x: x['expense_date'].strftime('%Y-%m') == dict['month'], amounts)
-    amount_filtered = filter(lambda x: (x[key] == key_pk), amount_filtered_month)
-    return sum(map(lambda x: x['total_price'], amount_filtered))
+def filter_sum(amounts, month, key=None, key_pk=None):
+    amount_filtered_month = filter(lambda x: x['expense_date'].strftime('%Y-%m') == month, amounts)
+    if key == None:
+        return sum(map(lambda x: x['total_price'], amount_filtered_month))
+    else:
+        amount_filtered = filter(lambda x: (x[key] == key_pk), amount_filtered_month)
+        return sum(map(lambda x: x['total_price'], amount_filtered))
 
-# 金額集計用関数
+# 月+αでの金額集計用関数
 def amount_aggregate(months, amounts, key, contents):
-    return_dicts = create_dict(months, key, contents)
-    for return_dict in return_dicts:
-        for content in return_dict['content']:
-            content['amount'] = filter_sum(return_dict, amounts, key, content[key].pk)
+    return_dicts = []
+    for month in months:
+        dict_month = {'month': month, 'contents': []}
+        for content in contents:
+            dict_month['contents'].append({'con_pk': content.pk,
+             'con_name': content.name,
+             'amount': filter_sum(amounts, month, key, content.pk)})
+        return_dicts.append(dict_month)
     return return_dicts
 
 # レコード集計画面
@@ -44,18 +45,23 @@ def record_aggregate():
     
     # 集計用の月一覧
     months = sorted_unique_list([amount['expense_date'].strftime('%Y-%m') for amount in amounts])
+    # 月で金額を集計
+    amounts_per_m = [{'month': month, 'amount': filter_sum(amounts, month)} for month in months]
     # 月・カテゴリで金額を集計
     amounts_per_m_c = amount_aggregate(months, amounts, 'category', categories)
-    # # 月・支払い方法で金額を集計
+    # 月・支払い方法で金額を集計
     amounts_per_m_p = amount_aggregate(months, amounts, 'payment', payments)
 
     context = {
         'amounts': amounts,
+        'categories': categories,
+        'payments': payments,
+        'amounts_per_m': amounts_per_m,
         'amounts_per_m_c': amounts_per_m_c,
         'amounts_per_m_p': amounts_per_m_p,
     }
-    return context
-    return amounts_per_m_c
+    # return context
+    return amounts_per_m
 
 ret = record_aggregate()
 print(ret)
--- expenses/views.py
+++ expenses/views.py
@@ -175,25 +175,25 @@ def payment_remove(request, pk):
 def sorted_unique_list(lst):
     return sorted(list(set(lst)))
 
-# レコード集計用の辞書作成関数
-def create_dict(months, key, contents):
-    ret_dict = [{'month': month, key: content, 'amount': 0}
-        for month in months
-          for content in contents]
-    return ret_dict
-
 # レコード集計用、フィルタ&合計関数
-def filter_sum(dict, amounts, key):
-    amount_filtered = filter(
-        lambda x: (x['expense_date'].strftime('%Y-%m') == dict['month']) and (x[key] == dict[key].pk),
-        amounts)
-    return sum(map(lambda x: x['total_price'], amount_filtered))
+def filter_sum(amounts, month, key=None, key_pk=None):
+    amount_filtered_month = filter(lambda x: x['expense_date'].strftime('%Y-%m') == month, amounts)
+    if key == None:
+        return sum(map(lambda x: x['total_price'], amount_filtered_month))
+    else:
+        amount_filtered = filter(lambda x: (x[key] == key_pk), amount_filtered_month)
+        return sum(map(lambda x: x['total_price'], amount_filtered))
 
-# 金額集計用関数
+# 月+αでの金額集計用関数
 def amount_aggregate(months, amounts, key, contents):
-    return_dicts = create_dict(months, key, contents)
-    for return_dict in return_dicts:
-        return_dict['amount'] = filter_sum(return_dict, amounts, key)
+    return_dicts = []
+    for month in months:
+        dict_month = {'month': month, 'contents': []}
+        for content in contents:
+            dict_month['contents'].append({'con_pk': content.pk,
+             'con_name': content.name,
+             'amount': filter_sum(amounts, month, key, content.pk)})
+        return_dicts.append(dict_month)
     return return_dicts
 
 # レコード集計画面
@@ -211,18 +211,22 @@ def record_aggregate(request):
     
     # 集計用の月一覧
     months = sorted_unique_list([amount['expense_date'].strftime('%Y-%m') for amount in amounts])
+    # 月で金額を集計
+    amounts_per_m = [{'month': month, 'amount': filter_sum(amounts, month)} for month in months]
     # 月・カテゴリで金額を集計
     amounts_per_m_c = amount_aggregate(months, amounts, 'category', categories)
-    # # 月・支払い方法で金額を集計
+    # 月・支払い方法で金額を集計
     amounts_per_m_p = amount_aggregate(months, amounts, 'payment', payments)
 
     context = {
         'amounts': amounts,
+        'categories': categories,
+        'payments': payments,
+        'amounts_per_m': amounts_per_m,
         'amounts_per_m_c': amounts_per_m_c,
         'amounts_per_m_p': amounts_per_m_p,
     }
     return render(request, 'expenses/record_aggregate.html', context)
-    # return context
 
 # カテゴリCSVインポート
 class RecordImport(generic.FormView):
